5fa6c083a95bf8556d6b62d1dd978feb7985ebde
Mario Alfonso Gutierrez Junca <mariojunca@hotmail.com>
2026-09-12 21:51:11 -0500

    Configuracion CORS para Front-End publico

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // Configuramos CORS para permitir la comunicación
 // entre el frontend de React y el backend de Express
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://comunika-api.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"]
 }));
